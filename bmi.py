fc8cfed697c9e474f5fb09458f183252fd63c6d7
phoo <PHOOPYAESA28.24@ichat.sp.edu.sg>
2025-05-26 09:54:19 +0800

    Bmi classtified version

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def calculate_bmi(height, weight): 
+    print("Height = " + str(height)) 
+    print("Weight = " + str(weight)) 
+    #Add code here to calculate BMI 
+    bmi = weight / (height*height)
+    #Add code here to display calculate BMI 
+    print(bmi) 
+    if bmi < 18.5 : 
+        print("Under Weight") 
+    elif (18.5 <= bmi <= 25.0) : 
+        print("Normal weight")
+    else :
+        print("Over Weight")
+
+calculate_bmi(weight=57, height=1.73)  
