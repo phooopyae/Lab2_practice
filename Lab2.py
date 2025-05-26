59d661be67c1eeb9b39b7abd08516405d301826e
phoo <PHOOPYAESA28.24@ichat.sp.edu.sg>
2025-05-26 10:14:01 +0800

    Exercise 4 included

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,39 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu() : 
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input() : 
+    num_in = input()
+    numList = num_in.split(",")
+    n_list = []
+    for num in numList: 
+        n_list.append(float(num))
+    return n_list
+
+
+def calc_average(numList) :
+    total = 0
+    avg = 0  
+    for num in numList: 
+        total += num
+    
+    avg = total / len(numList)
+
+    return avg
+def find_min_max(numList) : 
+    max_n = max(numList)
+    min_n = min(numList)
+    return ([min_n, max_n])
+
+#def sort_temperature() :
+
+#def calc_median_temperature() : 
+
+def main() : 
+    display_main_menu()
+    numList = get_user_input() 
+    print("Average : ", calc_average(numList))
+    print("Minimum and Maximum value : ", find_min_max(numList))
+
+if __name__ == "__main__" : 
+    main()
